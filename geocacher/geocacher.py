3627bcc6b8965a1361b93b19879a3dd2eb9789d3
Rob Wallace <rob@wallace.gen.nz>
2009-07-12 10:00:03 +1200

    Custom renderer added to display latitudes and longitudes in the configured format

--- geocacher/geocacher.py
+++ geocacher/geocacher.py
@@ -47,6 +47,56 @@ try:
 except:
     __version__ = "src"
 
+class DegRenderer(Grid.PyGridCellRenderer):
+    '''Renderer for cells containing measurements in degrees'''
+    def __init__(self, table, conf, mode = 'pure'):
+        Grid.PyGridCellRenderer.__init__(self)
+        self.conf = conf
+        self.table = table
+        self.mode = mode
+
+        self.colSize = None
+        self.rowSize = None
+
+    def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+        value = self.table.GetValue(row, col)
+        format = self.conf.common.coordFmt or 'hdd mm.mmm'
+        text = degToStr(value, format, self.mode)
+        hAlign, vAlign = attr.GetAlignment()
+        dc.SetFont(attr.GetFont())
+        if isSelected:
+            bg = grid.GetSelectionBackground()
+            fg = grid.GetSelectionForeground()
+        else:
+            bg = grid.GetDefaultCellBackgroundColour()
+            fg = grid.GetDefaultCellTextColour()
+
+        dc.SetTextBackground(bg)
+        dc.SetTextForeground(fg)
+        dc.SetBrush(wx.Brush(bg, wx.SOLID))
+        dc.SetPen(wx.TRANSPARENT_PEN)
+        dc.DrawRectangleRect(rect)
+        grid.DrawTextRectangle(dc, text, rect, hAlign, vAlign)
+
+    def GetBestSize(self, grid, attr, dc, row, col):
+        value = self.table.GetValue(row, col)
+        format = self.conf.common.coordFmt or 'hdd mm.mmm'
+        text = degToStr(value, format, self.mode)
+        w, h = dc.GetTextExtent(text)
+        return wx.Size(w, h)
+
+    def clone(self):
+        return DegRenderer(self.table, self.conf, self.mode)
+
+class LatRenderer(DegRenderer):
+    '''Renderer for cells containiny Latitudes (subcalss of DegRenderer)'''
+    def __init__(self, table, conf):
+        DegRenderer.__init__(self, table, conf, 'lat')
+
+class LonRenderer(DegRenderer):
+    '''Renderer for cells containiny Longitudes (subcalss of DegRenderer)'''
+    def __init__(self, table, conf):
+        DegRenderer.__init__(self, table, conf, 'lon')
 
 class ImageRenderer(Grid.PyGridCellRenderer):
     def __init__(self, table, conf):
@@ -220,6 +270,12 @@ class CacheDataTable(Grid.PyGridTableBase):
             'user_data4'  :Grid.GRID_VALUE_STRING}
 
         self.renderers = {
+            'lat'         :LatRenderer,
+            'lon'         :LonRenderer,
+            'cLat'        :LatRenderer,
+            'cLon'        :LonRenderer,
+            'oLat'        :LatRenderer,
+            'oLon'        :LonRenderer,
             'size'        :CacheSizeRenderer,
             'type'        :CacheTypeRenderer}
 
